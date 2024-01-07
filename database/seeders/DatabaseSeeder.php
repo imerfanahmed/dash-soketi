b373bc484cc886496dd6eafbf99f0ee739b8a794
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2024-01-07 18:19:41 +0600

    seeder class updated
    Signed-off-by: Erfan Ahmed Siam <erfan.siam98@gmail.com>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create( [
+            'name'     => 'Admin',
+            'email'    => 'admin@admin.com',
+            'password' => \Hash::make( 'password' )
+        ] );
 
         //create a app seeder
         App::create([
